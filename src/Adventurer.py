7c0cc7d376956582a33a98a3586304af9fec26ef
asutinl <austinnlee0@gmail.com>
2024-07-14 20:14:56 -0700

    Updated Adventurer and test class
    Updated toString to be more readable and adjusted test method to

--- src/Adventurer.py
+++ src/Adventurer.py
@@ -1,34 +1,35 @@
-#from src.Pillar import Pillar #in order to make a list of pillars
-from typing import List #allows use of list?
+# from src.Pillar import Pillar #in order to make a list of pillars
+from typing import List  # allows use of list?
+
+
 class Adventurer:
 
     def __init__(self, name, hit_points, total_healing_potions,
                  total_vision_potions) -> None:
-        #pillars_found: List[Pillar] --> add as last parameter later
+        # pillars_found: List[Pillar] --> add as last parameter later
         """Constructor for Adventurer Class"""
         self._name = name
-        self._hit_points = hit_points #75-100; ***should be randomly generated between 75 & 100***
+        self._hit_points = hit_points  # 75-100; ***should be randomly generated between 75 & 100***
         self._total_healing_potions = total_healing_potions
         self._total_vision_potions = total_vision_potions
-        #self.pillars_found: List[Pillar] = [] #list of pillar pieces found(4 total/possible)
-
+        # self.pillars_found: List[Pillar] = [] #list of pillar pieces found(4 total/possible)
 
     """
     Delete this guide later upon finishing the Adventurer class:
-    
+
     NOTABLE BEHAVIORS (from Adventurer portion of Course Project canvas page)
-    
+
         Adventurer/Hero & Dungeon will need to interact:
             When Adventurer walks into a room, if there is a potion
             in the room, the Adventurer automatically picks up the potion.
-            
+
                 (If want?, change pickup behavior AFTER this works)
-                
+
             Likewise, if there is a pit in the room, the Adventurer automatically
             falls in the pit and takes a Hit Point loss.
-            
+
             *These changes obviously affect the room*
-            
+
             For example, if the Adventurer walks into a room with a Healing
             Potion, the Adventurer will pick up the potion, changing the 
             Adventurers (healing) potion total, as well as changing the
@@ -36,14 +37,12 @@ class Adventurer:
     """
 
     def to_string(self) -> str:
-
         """PILLAR NOT INCLUDED YET SINCE NOT IMPLEMENTED YET, just testing
          if it is working for the other fields"""
 
-        #to do: format the string so it displays nicely (also change test method
-        #to account for it)
+        string = ("Name: " + self._name + "\n" +
+                  "Hit Points: " + str(self._hit_points) + "\n" +
+                  "Healing Potions: " + str(self._total_healing_potions) + "\n" +
+                  "Vision Potions: " + str(self._total_vision_potions))
 
-        string = (self._name + str(self._hit_points) + str(self._total_healing_potions) +
-                  str(self._total_vision_potions))
         return string
-
--- tests/test_Adventurer.py
+++ tests/test_Adventurer.py
@@ -10,15 +10,12 @@ def new_adventurer():
 def test_to_string():
     """This doesn't test the last parameter, list of pillars, but tests the other
     attributes."""
-    expected_string = ("John"
-                       "75"
-                       "1"
-                       "0"
-                       )
-
+    expected_string = ("Name: John\n"
+                       "Hit Points: 75\n"
+                       "Healing Potions: 1\n"
+                       "Vision Potions: 0")
     adventurer_one = Adventurer("John", 75, 1,
                                 0)
-
     actual_string = adventurer_one.to_string()
-
-    assert expected_string == actual_string
+    #NOTE: switching these around changes what is shown as expected vs actual
+    assert actual_string == expected_string
