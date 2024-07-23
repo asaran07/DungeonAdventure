2f7b6f407f2fa50c811e8a637b61e30e71037cb1
Kaely Willhite <nattea20@gmail.com>
2024-07-23 14:27:33 -0700

    Hero: Fleshed out block() method and created placeholder helper method block_damage()

--- src/characters/hero.py
+++ src/characters/hero.py
@@ -10,6 +10,20 @@ class Hero(DungeonCharacter):
         DungeonCharacter.__init__(name, hp, min_damage, max_damage,
                                   attack_speed, chance_to_hit)
 
-    def block(self):
 
+    def block(self):
         dice_roll_to_block = random.randint(1, 100)
+
+        if self.hp > 0 and dice_roll_to_block <= self.chance_to_block: # successful block
+            # ex: If hero has 90% chance to block, anything from 1 to 90 would block
+            self.block_damage()
+        elif self.hp > 0 and dice_roll_to_block > self.chance_to_block: # unsuccessful block
+            # ex: If hero has 90% chance to block, anything from 91 to 100 would fail to block
+            pass
+        else: # Hero already KOed
+            pass
+
+    def block_damage(self):
+        # need to change this to nullify damage from an attack once combat system is more
+        # fleshed out
+        pass
