e8a8bff45c6e567eb71af40206c5eaa031cfd6e9
asaran07 <saran.arsh07@icloud.com>
2024-07-23 12:16:52 -0700

    refactor(weapon.py): Remove get methods
    - Replace get methods with the Python 'property' decorator for simplicity
    - Add docstrings

--- src/items/weapon.py
+++ src/items/weapon.py
@@ -3,29 +3,41 @@ from src.items.item import Item
 
 
 class Weapon(Item):
-    def __init__(
-        self,
-        name: str,
-        description: str,
-        weight: float,
-        weapon_type: WeaponType,
-        damage: int,
-        durability: int = 100,
-    ):
+    """Represents a weapon item in the game"""
+
+    def __init__(self, name: str, description: str, weight: float, weapon_type: WeaponType, damage: int,
+                 durability: int = 100):
+        """
+        Create a weapon item
+
+        :param str name: Name of the weapon
+        :param str description: Description of the weapon
+        :param float weight: Weight of the weapon
+        :param WeaponType weapon_type: Type of the weapon
+        :param int damage: Damage the weapon can inflict
+        :param int durability: Durability of the weapon. Defaults to 100
+        """
         super().__init__(name, description, weight, ItemType.WEAPON)
         self._weapon_type = weapon_type
         self._damage = damage
         self._durability = durability
 
-    def get_weapon_type(self) -> WeaponType:
+    @property
+    def weapon_type(self) -> WeaponType:
+        """Weapon type of the weapon, e.g. Sword, Bow"""
         return self._weapon_type
 
-    def get_damage(self) -> int:
+    @property
+    def damage(self) -> int:
+        """Damage points the weapon can inflict"""
         return self._damage
 
-    def get_durability(self) -> int:
+    @property
+    def durability(self) -> int:
+        """Durability of the weapon."""
         return self._durability
 
     def use(self):
-        print(f"You swing the {self.get_name()}, dealing {self.get_damage()} damage!")
+        """Use the weapon to inflict damage, durability decreases by 1"""
+        print(f"You swing the {self.get_name()}, dealing {self._damage} damage!")
         self._durability -= 1
