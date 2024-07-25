7012833c123acd301cbb7f9fb1c65b58abf3b83c
asaran07 <saran.arsh07@icloud.com>
2024-07-25 11:42:22 -0700

    feat(Direction): Add from_string method to direction enum class
    - This new method will return the direction enum corresponding to the
    string value, or raise exception if there are no valid directions

--- src/enums/room_types.py
+++ src/enums/room_types.py
@@ -13,7 +13,15 @@ class RoomType(Enum):
 
 
 class Direction(Enum):
+    # TODO: Add an opposite direction method for cleaniness
     NORTH = "N"
     EAST = "E"
     SOUTH = "S"
     WEST = "W"
+
+    @classmethod
+    def from_string(cls, direction_str: str):
+        try:
+            return cls[direction_str.upper()]
+        except KeyError:
+            raise ValueError(f"'{direction_str}' is not a valid direction.")
