ce8add3d0e7b4e60fc05bf6d0c9fd2d8e4af78c7
asutinl <austinnlee0@gmail.com>
2024-07-29 14:57:41 -0700

    test(player): Refactoring methods
    - Reformat test methods to work with updated player class
    - Reformat test_player.py to become a test class to test all methods at once
    - Combine redundant test methods into one(add & remove item)

--- tests/test_player.py
+++ tests/test_player.py
@@ -5,99 +5,77 @@ from src.enums.item_types import ItemType
 from src.items.pillar import Pillar
 from src.items.potion import HealingPotion, VisionPotion
 
-@pytest.fixture
-def new_player():
-    player_one = Player()
-def test_to_string():
-    """Test method for to_string(). Requires player.inventory_to_string() to work in order to test."""
-    expected_string = ("Name: John\n"
-                       "Hit Points: 50\n"
-                       "Healing Potions: 1\n"
-                       "Vision Potions: 0\n"
-                       "Pillars Found: ")
-    adventurer_one = Player()
-    actual_string = adventurer_one.to_string()
-    # NOTE: switching these around changes what is shown as expected vs actual
-    assert actual_string == expected_string
 
-
-def test_use_health_potion():
-    """Test method for use_health_potion(). Requires to_string() to work in order to test."""
-    expected_string = ("Name: John\n"
-                       "Hit Points: 65\n"
-                       "Healing Potions: 0\n"
-                       "Vision Potions: 0\n"
-                       "Pillars Found: ")
-    adventurer_one = Player()
-    adventurer_one.use_health_potion()  # this should update Hit points AND number of healing potions
-    actual_string = adventurer_one.to_string()
-    # NOTE: switching these around changes what is shown as expected vs actual
-    assert actual_string == expected_string
-
-
-def test_add_and_drop_health_potion_from_inventory():
-    """Test method for adventurer_drop_item. Requires player.add_to_inventory() to work."""
-    expected_string = ("Name: John\n"
-                       "Hit Points: 50\n"
-                       "Healing Potions: 2\n"
-                       "Vision Potions: 0\n"
-                       "Pillars Found: ")
-    adventurer = Player()  # default value in parameter for Healing potions is 1
-    healing_potion = HealingPotion("healing_potion", "description", .5, 10)
-    adventurer.add_to_inventory(healing_potion)
-    adventurer.add_to_inventory(healing_potion)
-    adventurer.add_to_inventory(healing_potion)
-    adventurer.drop_from_inventory(healing_potion)
-    adventurer.drop_from_inventory(healing_potion)  # should be left with 2; 2 = 1 + 3 - 2
-    actual_string = adventurer.to_string()
-    assert actual_string == expected_string
-
-
-def test_add_and_drop_vision_potion():
-    """Test method for adding a vision potion."""
-    expected_string = ("Name: John\n"
-                       "Hit Points: 50\n"
-                       "Healing Potions: 1\n"
-                       "Vision Potions: 1\n"
-                       "Pillars Found: ")
-    adventurer = Player()  # default value in parameter for Healing potions is 1
-    vision_potion = VisionPotion("vision_potion", "description", .5)
-    adventurer.add_to_inventory(vision_potion)
-    adventurer.add_to_inventory(vision_potion)
-    adventurer.drop_from_inventory(vision_potion)
-    actual_string = adventurer.to_string()
-    assert actual_string == expected_string
-
-
-def test_drop_non_existent_items():
-    """Testing if drop_from_inventory accounts for dropping non-existent items."""
-    expected_string = ("Name: John\n"
-                       "Hit Points: 50\n"
-                       "Healing Potions: 0\n"
-                       "Vision Potions: 0\n"
-                       "Pillars Found: ")
-    adventurer = Player("John", 50, 1, 0, [])
-    healing_potion = HealingPotion()
+class TestPlayer:
+    # TODO: Write test methods for each item or includes each item.
+    health_potion = HealingPotion()
     vision_potion = VisionPotion()
     abstraction_pillar = Pillar("abstraction")
     encapsulation_pillar = Pillar("encapsulation")
     inheritance_pillar = Pillar("inheritance")
     polymorphism_pillar = Pillar("polymorphism")
 
-    adventurer.drop_from_inventory(healing_potion)
-    adventurer.drop_from_inventory(vision_potion)
-    adventurer.drop_from_inventory(abstraction_pillar)
-    adventurer.drop_from_inventory(encapsulation_pillar)
-    adventurer.drop_from_inventory(inheritance_pillar)
-    adventurer.drop_from_inventory(polymorphism_pillar)
-    actual_string = adventurer.to_string()
-    assert actual_string == expected_string
+    # pillar = Pillar() --> Update pillar class before testing pillar
 
+    @pytest.fixture
+    def new_player(self):
+        return Player("John", 50)
 
-def test_add_pillar():
-    """Test method for adding pillar."""
-    pass
+    def test_to_string(self, new_player: Player):
+        expected_string = ("Player: John\n"
+                           "HP: 50\n"
+                           "Inventory is empty")
+        adventurer_one = new_player
+        actual_string = adventurer_one.__str__()
+        # NOTE: switching these around changes what is shown as expected vs actual
+        assert actual_string == expected_string
 
+    def test_use_health_potion(self, new_player: Player):
+        expected_string = ("Player: John\n"
+                           "HP: 65\n"
+                           "Inventory is empty")
+        adventurer_one = new_player
 
-def test_drop_pillar():
-    pass
+        adventurer_one.add_to_inventory(self.health_potion)
+
+        # this should update Hit points AND number of healing potions from inventory
+        adventurer_one.use_item(self.health_potion.name)
+
+        actual_string = adventurer_one.__str__()
+        # NOTE: switching these around changes what is shown as expected vs actual
+        assert actual_string == expected_string
+
+    def test_add_and_remove_item_from_inventory(self, new_player: Player):
+        expected_string = ("Player: John\n"
+                           "HP: 50\n"
+                           "Inventory:\n"
+                           "  healing_potion: 1 (Weight: 0.5)\n"
+                           "  vision_potion: 2 (Weight: 1.0)\n"
+                           "Total Weight: 1.5/50.0")
+        adventurer_one = new_player
+
+        adventurer_one.add_to_inventory(self.health_potion)
+        adventurer_one.add_to_inventory(self.health_potion)
+        adventurer_one.add_to_inventory(self.health_potion)
+        adventurer_one.remove_from_inventory(self.health_potion.name)
+        adventurer_one.remove_from_inventory(self.health_potion.name)
+
+        adventurer_one.add_to_inventory(self.vision_potion)
+        adventurer_one.add_to_inventory(self.vision_potion)
+        adventurer_one.add_to_inventory(self.vision_potion)
+        adventurer_one.remove_from_inventory(self.vision_potion.name)
+
+        # TODO: Add and test pillar item
+
+        actual_string = adventurer_one.__str__()
+        assert actual_string == expected_string
+
+    def test_remove_non_existent_items(self, new_player: Player):
+        """Testing if removing from inventory accounts for dropping non-existent items."""
+        expected_string = ("Player: John\n"
+                           "HP: 50\n"
+                           "Inventory is empty")
+        adventurer_one = new_player
+        adventurer_one.remove_from_inventory(self.health_potion.name)
+        actual_string = adventurer_one.__str__()
+        assert actual_string == expected_string
