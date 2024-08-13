e3304add98beac595723113a1222bf11a430be74
Kaely Willhite <nattea20@gmail.com>
2024-08-13 14:15:32 -0700

    Cleaning up controls_display.py

--- src/dungeon_adventure/views/pygame/room/controls_display.py
+++ src/dungeon_adventure/views/pygame/room/controls_display.py
@@ -3,11 +3,11 @@ import pygame
 
 class ControlsDisplay:
     def __init__(self, screen_width: int, screen_height: int):
-        self.controls_width = int(screen_width * 0.2)  # 20% of screen width
-        self.controls_height = int(screen_height * 0.3)  # 30% of screen height
+        self.controls_width = int(screen_width * 0.15)  # 15% of screen width
+        self.controls_height = int(screen_height * 0.25)  # 25% of screen height
         self.controls_rect = pygame.Rect(
             screen_width - self.controls_width - 10,
-            screen_height - self.controls_height - 10,
+            screen_height - self.controls_height - 315,
             self.controls_width,
             self.controls_height,
         )
@@ -29,7 +29,7 @@ class ControlsDisplay:
         # Draw title
         title_surface = self.font.render("Controls", True, (255, 255, 255))
         surface.blit(
-            title_surface, (self.controls_rect.x + 10, self.controls_rect.y + 10)
+            title_surface, (self.controls_rect.x + 70, self.controls_rect.y + 10)
         )
 
         # Draw keybindings
@@ -38,4 +38,4 @@ class ControlsDisplay:
             value_surface = self.font.render(value, True, (255, 255, 255))
             y_pos = self.controls_rect.y + 50 + i * 30
             surface.blit(key_surface, (self.controls_rect.x + 20, y_pos))
-            surface.blit(value_surface, (self.controls_rect.x + 80, y_pos))
+            surface.blit(value_surface, (self.controls_rect.x + 100, y_pos))
