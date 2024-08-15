c439ffe0956246b1cbb0b12bd84a9297cec28752
asaran07 <saran.arsh07@icloud.com>
2024-08-15 11:50:52 -0700

    Add new Panel creation methods
    - Add `create_panel` and `add_border` methods in `panel.py`
    - Include center alignment utility in `Panel` class
    - Implement logging in `TestScreen` class
    - Adjust dimensions and initialization of `TestScreen`
    - Refactor main loop to utilize updated `draw` method and Pygame initialization

--- src/dungeon_adventure/views/pygame/UI/panel.py
+++ src/dungeon_adventure/views/pygame/UI/panel.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import pygame
 
 from dungeon_adventure.views.pygame.UI.ui_element import UIElement
@@ -7,8 +9,14 @@ class Panel(UIElement):
     def __init__(self, width: int, height: int):
         self._width = width
         self._height = height
-        self._surface = pygame.Surface((self.width, self.height))
+        self._surface = pygame.Surface(self.size)
         self._frame = pygame.Rect(self.surface.get_rect())
+
+    def create_panel(self, fill_color: Tuple[int, int, int]):
+        pygame.draw.rect(self.surface, fill_color, self._frame)
+
+    def add_border(self, border_width: int, border_color: Tuple[int, int, int]):
+        pygame.draw.rect(self.surface, border_color, self._frame, border_width)
 
     @property
     def width(self) -> int:
@@ -25,3 +33,6 @@ class Panel(UIElement):
     @property
     def frame(self) -> pygame.Rect:
         return self._frame
+
+    def center_of(self, surface: pygame.Surface) -> pygame.Rect:
+        return self.surface.get_rect(center=surface.get_rect().center)
